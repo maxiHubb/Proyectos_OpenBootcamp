25405ae18e9e5cd0e7d25fb9992f234c96f8db8f
Maximiliano Rabenko <mrabenko@alumno.unlam.edu.ar>
2023-02-13 23:40:14 -0300

    Ejercicio 3

---
+++ src/com/Ejercicio3/Ejercicio3DeOpenBootcamp.java
@@ -0,0 +1,13 @@
+package com.Ejercicio3;
+
+public class Ejercicio3DeOpenBootcamp {
+
+    public static void main(String[] args) {
+
+        String[] colores = {"Rojo", "Amarillo", "Verde", "Azul"};
+
+        for (String color : colores) {
+            System.out.println("Los colores son: " + color);
+        }
+    }
+}
